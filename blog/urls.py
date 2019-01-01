0b70768388922cb4f8072810befa5fef673d547b
kunal-visoulia <kunalvisoulia@gmail.com>
2019-01-01 20:29:26 +0530

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+
 ]
 #URL: http://127.0.0.1:8000/post/1/
 #Django expects an integer value and will transfer 
