e792f087f8dc218c30372ee3efb108174adf00ab
kunal-visoulia <kunalvisoulia@gmail.com>
2019-01-01 20:08:59 +0530

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
+#URL: http://127.0.0.1:8000/post/1/
+#Django expects an integer value and will transfer 
+# it to a view as a variable called pk.
