5bc3639a038d0e30d492fc7bcf79ff68edc294e4
kunal-visoulia <kunalvisoulia@gmail.com>
2019-01-02 14:54:40 +0530

    Adding secure login to Blog

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,3 +122,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+# when the login page is accessed directly, it will redirect a successful login to the top-level index (the homepage of our blog).
+LOGIN_REDIRECT_URL = '/'
